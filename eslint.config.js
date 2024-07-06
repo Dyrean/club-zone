2310b621ddee71cce0ab4178e1ab2f0ce9dce141
Dyrean <fcanbeyy@gmail.com>
2024-07-06 19:03:22 +0300

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,42 @@
 import antfu from "@antfu/eslint-config"
 
-export default antfu({
-	stylistic: {
-		indent: "tab", // 4, or 'tab'
-		quotes: "double", // or 'double'
+import { FlatCompat } from "@eslint/eslintrc"
+
+const compat = new FlatCompat()
+
+export default antfu(
+	{
+		stylistic: {
+			indent: "tab", // 4, or 'tab'
+			quotes: "double", // or 'double'
+		},
+
+		typescript: true,
+		vue: true,
+
+		jsonc: true,
+		yaml: true,
+
+		formatters: {
+			/**
+			 * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+			 * By default uses Prettier
+			 */
+			css: true,
+			/**
+			 * Format HTML files
+			 * By default uses Prettier
+			 */
+			html: true,
+			/**
+			 * Format Markdown files
+			 * Supports Prettier and dprint
+			 * By default uses Prettier
+			 */
+			markdown: "prettier",
+		},
 	},
-
-	typescript: true,
-	vue: true,
-
-	// Disable jsonc and yaml support
-	jsonc: true,
-	yaml: true,
-})
+	...compat.config({
+		extends: ["prettier"],
+	}),
+)
