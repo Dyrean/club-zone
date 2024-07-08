8343221c5770805602c0087c24079f46869d8b2c
Dyrean <fcanbeyy@gmail.com>
2024-07-09 00:21:14 +0300

    lucia auth general setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,33 @@
 import antfu from "@antfu/eslint-config"
 
-import { FlatCompat } from "@eslint/eslintrc"
-
-const compat = new FlatCompat()
-
-export default antfu(
-	{
-		stylistic: {
-			indent: "tab", // 4, or 'tab'
-			quotes: "double", // or 'double'
-		},
-
-		typescript: true,
-		vue: true,
-
-		jsonc: true,
-		yaml: true,
-
-		formatters: {
-			/**
-			 * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
-			 * By default uses Prettier
-			 */
-			css: true,
-			/**
-			 * Format HTML files
-			 * By default uses Prettier
-			 */
-			html: true,
-			/**
-			 * Format Markdown files
-			 * Supports Prettier and dprint
-			 * By default uses Prettier
-			 */
-			markdown: "prettier",
-		},
+export default antfu({
+	stylistic: {
+		indent: "tab", // 4, or 'tab'
+		quotes: "double", // or 'double'
 	},
-	...compat.config({
-		extends: ["prettier"],
-	}),
-)
+
+	typescript: true,
+	vue: true,
+
+	jsonc: true,
+	yaml: true,
+
+	formatters: {
+		/**
+		 * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+		 * By default uses Prettier
+		 */
+		css: true,
+		/**
+		 * Format HTML files
+		 * By default uses Prettier
+		 */
+		html: true,
+		/**
+		 * Format Markdown files
+		 * Supports Prettier and dprint
+		 * By default uses Prettier
+		 */
+		markdown: "prettier",
+	},
+})
---
+++ types/auth.ts
@@ -0,0 +1,19 @@
+import { email, minLength, object, optional, picklist, string } from "valibot"
+
+export const SignupSchema = object({
+	email: string([email("Invalid email address")]),
+	username: string([
+		minLength(2, "Username must be at least 2 characters long"),
+	]),
+	password: string([
+		minLength(8, "Password must be at least 8 characters long"),
+	]),
+	role: optional(picklist(["user", "admin"])),
+})
+
+export const LoginSchema = object({
+	email: string([email("Invalid email address")]),
+	password: string([
+		minLength(8, "Password must be at least 8 characters long"),
+	]),
+})
