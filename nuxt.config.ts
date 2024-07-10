fa5d5b4338bc329b1f6700de6435c12dc411e6f2
Dyrean <fcanbeyy@gmail.com>
2024-07-10 23:41:34 +0300

    pinia added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
 		},
 	},
 
-	modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+	modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@pinia/nuxt"],
 
 	shadcn: {
 		/**
