5e67d5c62870f42c0b3e824d584984cd02746b40
Dyrean <fcanbeyy@gmail.com>
2024-07-10 23:48:56 +0300

    vueuse added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
 		},
 	},
 
-	modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@pinia/nuxt"],
+	modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@pinia/nuxt", "@vueuse/nuxt"],
 
 	shadcn: {
 		/**
