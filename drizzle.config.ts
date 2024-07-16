e99bf93dccfd4939809a2cfa52e794bf8a275cc1
Dyrean <fcanbeyy@gmail.com>
2024-07-17 00:13:13 +0300

    turso added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,13 @@
+/* eslint-disable node/prefer-global/process */
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
 	driver: "turso",
+	dialect: "sqlite",
 	schema: "./server/db/schema.ts",
 	out: "./server/db/migrations",
 	dbCredentials: {
-		url: "file:./server/database.db",
+		url: process.env.TURSO_CONNECTION_URL!,
+		authToken: process.env.TURSO_AUTH_TOKEN!,
 	},
-	dialect: "sqlite",
 })
--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,3 +1,4 @@
+/* eslint-disable node/prefer-global/process */
 import { createClient } from "@libsql/client"
 import { type LibSQLDatabase, drizzle } from "drizzle-orm/libsql"
 import * as schema from "~/server/db/schema"
@@ -11,7 +12,8 @@ let _db: LibSQLDatabase<typeof schema> | null = null
 export function useDB() {
 	if (!_db) {
 		const libsql = createClient({
-			url: "file:server/database.db",
+			url: process.env.TURSO_CONNECTION_URL!,
+			authToken: process.env.TURSO_AUTH_TOKEN!,
 		})
 		_db = drizzle(libsql, { schema })
 	}
